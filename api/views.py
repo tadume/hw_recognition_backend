4c91fee7dab1d45f53a20ea99bf410c0d32f8d27
田爪大智 <>
2024-08-11 11:56:42 +0900

    response predict data

--- api/views.py
+++ api/views.py
@@ -4,10 +4,20 @@ from django.views.decorators.csrf import csrf_exempt
 import json
 
 from api.tensorflow import image_preprocessing
+from api.tensorflow import predict_mnist
+
 @csrf_exempt
 def predict(request):
     body = json.loads(request.body)
     base64image = body["image"].split(",")[1]
     numpy_image = image_preprocessing.base64_to_numpy(base64image)
+    predicted_label, prediction_prob = predict_mnist.predict(numpy_image)
 
-    return HttpResponse(status=200)
+    response_body = {
+        "predicted_label": str(predicted_label),
+        "prediction_prob": prediction_prob
+    }
+
+    response_json = json.dumps(response_body, ensure_ascii=False, indent=2)
+
+    return HttpResponse(response_json, status=200)
